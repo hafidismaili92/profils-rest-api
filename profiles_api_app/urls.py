0289aaec0bfaa8e4172fb2e001c3ffe1362065d2
hafid ismaili <hafid.ismaili92@gmail.com>
2023-11-16 10:57:18 +0000

    added profiles viewset

--- profiles_api_app/urls.py
+++ profiles_api_app/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_api_app import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('test-viewset',views.Testapiviewsets, basename='test-viewset')
+router.register('profile',views.UserProfilViewSet)
 urlpatterns = [
     path('testApiView/', views.TestApiView.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -1,7 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import viewsets
+
+
+
+
 from profiles_api_app import serializers
+from profiles_api_app import models
+from profiles_api_app import permissions
 
 
 class TestApiView(APIView):
@@ -20,12 +27,55 @@ class TestApiView(APIView):
             return Response({'message':f'hello dear {name}'})
         else:
             return Response(serializer.errors,status=400)
-    def put(self,reuest,pk=None):
+    def put(self,request,pk=None):
         """hndle put request"""
         return Response({'message':'server recieve a put request'})
-    def patch(self,reuest,pk=None):
+    
+    def patch(self,request,pk=None):
         """hndle patch request"""
         return Response({'message':'server recieve a patch request'})
-    def delete(self,reuest,pk=None):
+    
+    def delete(self,request,pk=None):
         """hndle delete request"""
         return Response({'message':'server recieve a delete request'})
+    
+class Testapiviewsets(viewsets.ViewSet):
+    """a class to test aapiviewSets """
+
+    serializer_class = serializers.testApiSerializer
+
+
+    def list(self,request):
+        """handl list of all objects"""
+        randomData = ['res 1','res 2','res 3']
+        return Response({'message':'success','data':randomData})
+    
+    def create(self,request):
+        """handle creating aan object"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message':f'hello dear {name}'})
+        else:
+            return Response(serializer.errors,status=400)
+    def retrieve(self,request,pk=None):
+        """hndle getting one  object"""
+        return Response({'message':'server recieve a get one object request'})
+    def update(self,request,pk=None):
+        """hndle updateing an object"""
+        return Response({'message':'server recieve a update object request'})
+    def partial_update(self,request,pk=None):
+        """hndle partial update of object"""
+        return Response({'message':'server recieve a partial_update object request'})
+    
+    def destroy(self,request,pk=None):
+        """hndle destroy object"""
+        return Response({'message':'server recieve a destroy object request'})
+
+
+class UserProfilViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.userProfilSerilizer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
