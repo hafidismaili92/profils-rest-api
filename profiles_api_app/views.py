9887cfb1b1520ed53e0acc41e504663c8e2a416a
hafid ismaili <hafid.ismaili92@gmail.com>
2023-11-16 11:35:27 +0000

    added searching to user profile api

--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
-
+from rest_framework import filters
 
 
 
@@ -79,3 +79,5 @@ class UserProfilViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
